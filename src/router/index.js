5d9273b7c09132d5862c59fc16fbd7978cfb6b51
xusheng <695644578@qq.com>
2026-03-13 10:55:14 +0800

    fix 路由写错

--- src/router/index.js
+++ src/router/index.js
@@ -67,7 +67,7 @@ const router = createRouter({
   history: createWebHistory(
     import.meta.env.BASE_URL),
   routes: [
-    HomeRouter,
+    homeRouter,
     {
       path: '/login',
       name: 'Login',
