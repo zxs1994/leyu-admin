bbc611543d0c7e4bcb533510fd0137074841134b
xusheng <695644578@qq.com>
2026-04-21 12:21:50 +0800

    重构Content组件，移除不必要的keepAlive逻辑

--- src/layout/components/Content.vue
+++ src/layout/components/Content.vue
@@ -1,28 +1,20 @@
-<script setup>
-import { ref, watchEffect } from 'vue'
-import { useRoute } from 'vue-router'
-const route = useRoute()
-
-const keepAliveInclude = ref([])
-watchEffect(route, () => {
-	if (route.meta?.keepAlive) {
-		keepAliveInclude.value.push(route.name)
-		keepAliveInclude.value = Array.from(new Set(keepAliveInclude.value))
-		console.log(keepAliveInclude.value)
-	}
-})
-</script>
+<script setup></script>
 <template>
 	<a-layout-content class="layout-content">
 		<RouterView v-slot="{ Component, route }">
-			<KeepAlive :include="keepAliveInclude">
+			<KeepAlive>
 				<component
+					v-if="route.meta?.keepAlive"
 					:is="Component"
-					:key="$route.fullPath"
+					:key="route.name || route.path"
 					class="CONTENT" />
 			</KeepAlive>
+			<component
+				v-if="!route.meta?.keepAlive"
+				:is="Component"
+				:key="route.fullPath"
+				class="CONTENT" />
 		</RouterView>
-		<!-- <RouterView /> -->
 	</a-layout-content>
 </template>
 <style lang="less" scoped>
